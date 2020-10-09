2733175c273e39cdde9c92691c76a26db589a8fa
mmuginov <mmuginov@protonmail.com>
2020-10-09 11:58:39 -0700

    BluePrint and BluePrint tests

--- Core/GameObjects/Abstracts/Blueprint.cs
+++ Core/GameObjects/Abstracts/Blueprint.cs
@@ -51,7 +51,8 @@ namespace Emberpoint.Core.GameObjects.Abstracts
         {
             var name = GetType().Name;
             var blueprintPath = Path.Combine(BlueprintPath, name + ".txt");
-            var blueprintConfigPath = Path.Combine(Constants.Blueprint.BlueprintsConfigPath, Constants.Blueprint.BlueprintTiles + ".json");
+            var blueprintConfigPath = 
+                Path.Combine(Constants.Blueprint.BlueprintsConfigPath, Constants.Blueprint.BlueprintTiles + ".json");
 
             if (!File.Exists(blueprintPath) || !File.Exists(blueprintConfigPath) || !File.Exists(Constants.Blueprint.SpecialCharactersPath)) 
                 return Array.Empty<T>();
@@ -127,7 +128,114 @@ namespace Emberpoint.Core.GameObjects.Abstracts
             return cells.ToArray();
         }
 
-        private Color GetColorByString(string value)
+        public T[] GetCellsRefactored()
+        {
+            var name = GetType().Name;
+            var blueprintPath = Path.Combine(BlueprintPath, name + ".txt");
+            var blueprintConfigPath = GetBlueprintConfigPath();
+
+            if (!AllConfigPathsExist(blueprintPath, blueprintConfigPath, Constants.Blueprint.SpecialCharactersPath))
+                return Array.Empty<T>();
+
+            var specialConfig = JsonConvert.DeserializeObject<BlueprintConfig>(File.ReadAllText(Constants.Blueprint.SpecialCharactersPath));
+            var specialChars = specialConfig.Tiles.ToDictionary(a => a.Glyph, a => a);
+
+            var config = JsonConvert.DeserializeObject<BlueprintConfig>(File.ReadAllText(blueprintConfigPath));
+            var tiles = config.Tiles.ToDictionary(a => a.Glyph, a => a);
+
+            CheckForReservedChars(tiles, specialChars, name);
+
+            var blueprint = File.ReadAllText(blueprintPath).Replace("\r", string.Empty).Split('\n');
+
+            var nullTile = BlueprintTile.Null();
+            var cells = GetCells(blueprint, nullTile, tiles, name);
+
+            return cells.ToArray();
+        }
+
+        private static void CheckForReservedChars(Dictionary<char, BlueprintTile> tiles, IReadOnlyDictionary<char, BlueprintTile> specialChars, string name)
+        {
+            var containedKeys = tiles.Keys.Where(specialChars.ContainsKey);
+            if (containedKeys.Any())
+            {
+                throw new Exception("Glyphs " + string.Join('\n', containedKeys.Select(key => $"'{key}'")) +
+                                    "are reserved as a special character and cannot be used in " + name);
+            }
+        }
+
+        private IEnumerable<T> GetCells(IReadOnlyList<string> blueprint, 
+            BlueprintTile nullTile, 
+            IReadOnlyDictionary<char, BlueprintTile> tiles, string name)
+        {
+            for (int y = 0; y < GridSizeY; y++)
+            {
+                for (int x = 0; x < GridSizeX; x++)
+                {
+                    var tile = nullTile;
+                    if (TryGetCharValue(blueprint, x, y, out var charValue))
+                        if (!tiles.TryGetValue(charValue, out tile))
+                            throw new Exception("Glyph '" + charValue + 
+                                                "' was not present in the config file for blueprint: " + name);
+
+                    var position = new Point(x, y);
+                    var cell = ConstructCell(tile, position);
+
+                    if (string.IsNullOrWhiteSpace(tile.LightColor) == false)
+                        cell.LightProperties.LightColor = GetColorByString(tile.LightColor);
+
+                    yield return cell;
+                }
+            }
+        }
+
+        private static bool TryGetCharValue(IReadOnlyList<string> blueprint, int x, int y, out char charValue)
+        {
+            if (y >= blueprint.Count || x >= blueprint[y].Length)
+            {
+                charValue = ' ';
+                return false;
+            }
+            charValue = blueprint[y][x];
+            return true;
+        }
+
+        private static T ConstructCell(BlueprintTile tile, Point position)
+        {
+            var foregroundColor = GetColorByString(tile.Foreground);
+            var backgroundColor = tile.Background != null ? GetColorByString(tile.Background) : Color.Black;
+            return new T
+            {
+                Glyph = tile.Glyph,
+                Position = position,
+                Foreground = foregroundColor,
+                Background = backgroundColor,
+                CellProperties = new EmberCell.EmberCellProperties
+                {
+                    NormalForeground = foregroundColor,
+                    NormalBackground = backgroundColor,
+                    ForegroundFov = Color.Lerp(foregroundColor, Color.Black, .5f),
+                    BackgroundFov = Color.Lerp(backgroundColor, Color.Black, .5f),
+                    Walkable = tile.Walkable,
+                    Name = tile.Name,
+                    BlocksFov = tile.BlocksFov,
+                },
+                LightProperties = new EmberCell.LightEngineProperties
+                {
+                    EmitsLight = tile.EmitsLight,
+                    LightRadius = tile.LightRadius,
+                    Brightness = tile.Brightness
+                }
+            };
+        }
+
+        private bool AllConfigPathsExist(params string[] configPaths) => configPaths.All(File.Exists);
+
+        private string GetBlueprintConfigPath()
+        {
+            return Path.Combine(Constants.Blueprint.BlueprintsConfigPath, Constants.Blueprint.BlueprintTiles + ".json");
+        }
+
+        private static Color GetColorByString(string value)
         {
             var prop = typeof(Color).GetProperty(value);
             if (prop != null)
--- Tests/BlueprintTests.cs
+++ Tests/BlueprintTests.cs
@@ -2,6 +2,13 @@
 using NUnit.Framework;
 using Tests.TestObjects.Blueprints;
 using Tests.TestObjects.Grids;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Emberpoint.Core;
+using Emberpoint.Core.GameObjects.Abstracts;
+using Emberpoint.Core.GameObjects.Map;
+using NuGet.Frameworks;
 
 namespace Tests
 {
@@ -24,6 +31,58 @@ namespace Tests
             var cells = _grid.Blueprint.GetCells();
             Assert.IsNotNull(cells);
             Assert.IsTrue(cells.Length > 0);
+        }
+
+        [Test]
+        public void RefactoredMethodIdentical()
+        {
+            var cells = _grid.Blueprint.GetCells();
+            var cellsRefactored = _grid.Blueprint.GetCellsRefactored();
+
+            Assert.IsTrue(cells.Length == cellsRefactored.Length);
+            Assert.IsTrue(GetComparisons(cells, cellsRefactored).All(b=>b));
+        }
+
+        private IEnumerable<bool> GetComparisons(IEnumerable<EmberCell> cells1, IEnumerable<EmberCell> cells2)
+        {
+            var enumerator1 = cells1.GetEnumerator();
+            var enumerator2 = cells2.GetEnumerator();
+
+            while (enumerator1.MoveNext())
+            {
+                enumerator2.MoveNext();
+                yield return enumerator1.Current != null && 
+                             enumerator1.Current.Equals(enumerator2.Current);
+            }
+
+            enumerator1.Dispose();
+            enumerator2.Dispose();
+        }
+
+        [Test]
+        public void BlueprintToCells_ReturnsCorrectCells_BasedOnTxtFile()
+        {
+            var blueprintPath = Path.Combine(Constants.Blueprint.TestBlueprintsPath, _grid.Blueprint.GetType().Name + ".txt");
+            var blueprint = File.ReadAllText(blueprintPath).Replace("\r", "").Split('\n');
+
+            var nullTile = BlueprintTile.Null();
+            for (int y = 0; y < _grid.GridSizeY; y++)
+            {
+                for (int x = 0; x < _grid.GridSizeX; x++)
+                {
+                    char charValue;
+                    if (y >= blueprint.Length || x >= blueprint[y].Length)
+                    {
+                        charValue = nullTile.Glyph;
+                    }
+                    else
+                    {
+                        charValue = blueprint[y][x];
+                    }
+
+                    Assert.IsTrue(_grid.GetCell(x, y).Glyph == charValue);
+                }
+            }
         }
     }
 }
