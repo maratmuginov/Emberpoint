57297040b1588d4046b547df8aea53d925140d4d
Venomous <stefvandenbruel@outlook.com>
2020-10-09 18:25:27 +0200

    Issue #30: EmberCell Effects (#100)

--- Core/GameObjects/Interfaces/IEntity.cs
+++ Core/GameObjects/Interfaces/IEntity.cs
@@ -3,12 +3,13 @@ using Microsoft.Xna.Framework;
 
 namespace Emberpoint.Core.GameObjects.Interfaces
 {
-    public interface IEntity : IRenderable
+    public interface IEntity : IRenderable, IEntityBaseStats
     {
         int ObjectId { get; }
         FOV FieldOfView { get; }
         int FieldOfViewRadius { get; set; }
         Point Position { get; set; }
+        int Glyph { get; }
         void ResetFieldOfView();
         void MoveTowards(Point position, bool checkCanMove = true);
         bool CanMoveTowards(Point position);
